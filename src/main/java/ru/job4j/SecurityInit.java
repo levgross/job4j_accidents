d01f09fc16e67f1a14b1a2978528f84df353cbc8
Grossevich Lev <levgross@gmail.com>
2022-12-12 11:32:51 +0400

    Revert "0. Spring Security [#6879]"
    This reverts commit 4f77d8e8bc551bfc76c9924cae6c872bad598619.

--- src/main/java/ru/job4j/SecurityInit.java
+++
@@ -1,6 +0,0 @@
-package ru.job4j;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
-}

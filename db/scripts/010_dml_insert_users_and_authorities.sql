516595ed2222483c02e15a444b7f552c7ff5d900
Grossevich Lev <levgross@gmail.com>
2022-12-16 13:53:05 +0400

    Обновил README Добавил скрипты на таблицы users, authorities

--- db/scripts/010_dml_insert_users_and_authorities.sql
+++ db/scripts/010_dml_insert_users_and_authorities.sql
@@ -1,16 +1,3 @@
-CREATE TABLE authorities (
-  id serial primary key,
-  authority VARCHAR(50) NOT NULL unique
-);
-
-CREATE TABLE users (
-  id serial primary key,
-  username VARCHAR(50) NOT NULL unique,
-  password VARCHAR(100) NOT NULL,
-  enabled boolean default true,
-  authority_id int not null references authorities(id)
-);
-
 insert into authorities (authority) values ('ROLE_USER');
 insert into authorities (authority) values ('ROLE_ADMIN');
 
